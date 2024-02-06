2e65b07856e2d88cd706adcd623d84fde17e946b
Thomas Browning <tb65536@uw.edu>
2024-02-06 12:36:43 -0800

    new code

--- Julia/AWS.jl
+++ Julia/AWS.jl
@@ -138,14 +138,11 @@ Li3 = Complex{Int}[
     0     0     0     0     0     0     0     2
 ]
 
-# Build up G row by row, checking to make sure that the congruence conditions are satisfied
-# Keep track of T so far, and Li G so far
-
 function Base.:÷(z::Complex{Int}, m::Int)
     return Complex(z.re ÷ m, z.im ÷ m)
 end
 
-function Base.:÷(M::Matrix{Complex{Int}}, m::Int)
+function Base.:÷(M::Vector{Complex{Int}}, m::Int)
     return M .÷ m
 end
 
@@ -153,11 +150,12 @@ function Base.:%(z::Complex{Int}, m::Int)
     return Complex(mod(z.re, m), mod(z.im, m))
 end
 
-function Base.:%(M::Matrix{Complex{Int}}, m::Int)
+function Base.:%(M::Vector{Complex{Int}}, m::Int)
     return M .% m
 end
 
-function run(n::Int, tracebound::Int, S::Matrix{Complex{Int}}, D::Matrix{Int}, L::Matrix{Complex{Int}}, Li::Matrix{Complex{Int}}, d::Int)
+# returns all vectors with norm k (k should be a nonnegative even integer)
+function search(k::Int, S::Matrix{Complex{Int}}, D::Matrix{Int}, L::Matrix{Complex{Int}}, Li::Matrix{Complex{Int}}, d::Int)
     @assert ishermitian(S)
     @assert isposdef(S)
     @assert isdiag(D)
@@ -166,97 +164,125 @@ function run(n::Int, tracebound::Int, S::Matrix{Complex{Int}}, D::Matrix{Int}, L
     @assert L' * D * L == d * S
     @assert Li * L == d * I
     @assert L * Li == d * I
-    m0 = Dict{Pair{Matrix{Complex{Int}}, Matrix{Complex{Int}}}, BigInt}() # (G' D G = d T, Li G (mod d)) ↦ a
-    m0[Pair(zeros(Complex{Int}, n, n), zeros(Complex{Int}, 8, n))] = 1
-    for i in 8:-1:1
-        println("status: ", i, " (", length(m0), ")")
-        m1 = Dict{Pair{Matrix{Complex{Int}}, Matrix{Complex{Int}}}, BigInt}()
-        for ((dT, LiG), a) in m0
-            bound = d * tracebound
-            for j in 1:n
-                bound -= convert(Int, dT[j, j])
+    normbound = d * k
+    zlist = Complex{Int}[]
+    for x in range(-normbound, normbound)
+        for y in range(-normbound, normbound)
+            z = x + y * im
+            if abs2(z) ≤ normbound
+                push!(zlist, z)
             end
-            bound ÷= D[i, i]
-            s = isqrt(bound)
-            possibilities = [[] for j in 1:n]
-            for x in -s:s
-                for y in -s:s
-                    z = x + y * im
-                    if abs2(z) > bound
+        end
+    end
+    Glist = Vector{Vector{Complex{Int}}}()
+    for z8 in zlist
+        if D[8,8] * abs2(z8) > normbound
+            continue
+        end
+        if (Li[8,8] * z8) % d != 0
+            continue
+        end
+        for z7 in zlist
+            if D[7,7] * abs2(z7) + D[8,8] * abs2(z8) > normbound
+                continue
+            end
+            if (Li[7,7] * z7 + Li[7,8] * z8) % d != 0
+                continue
+            end
+            for z6 in zlist
+                if D[6,6] * abs2(z6) + D[7,7] * abs2(z7) + D[8,8] * abs2(z8) > normbound
+                    continue
+                end
+                if (Li[6,6] * z6 + Li[6,7] * z7 + Li[6,8] * z8) % d != 0
+                    continue
+                end
+                for z5 in zlist
+                    if D[5,5] * abs2(z5) + D[6,6] * abs2(z6) + D[7,7] * abs2(z7) + D[8,8] * abs2(z8) > normbound
                         continue
                     end
-                    for j in 1:n
-                        if (LiG[i, j] + Li[i, i] * z) % d == 0
-                            push!(possibilities[j], z)
-                        end
+                    if (Li[5,5] * z5 + Li[5,6] * z6 + Li[5,7] * z7 + Li[5,8] * z8) % d != 0
+                        continue
                     end
-                end
-            end
-            @assert n == 4
-            for z1 in possibilities[1]
-                for z2 in possibilities[2]
-                    for z3 in possibilities[3]
-                        for z4 in possibilities[4]
-                            if abs2(z1) + abs2(z2) + abs2(z3) + abs2(z4) > bound
+                    for z4 in zlist
+                        if D[4,4] * abs2(z4) + D[5,5] * abs2(z5) + D[6,6] * abs2(z6) + D[7,7] * abs2(z7) + D[8,8] * abs2(z8) > normbound
+                            continue
+                        end
+                        if (Li[4,4] * z4 + Li[4,5] * z5 + Li[4,6] * z6 + Li[4,7] * z7 + Li[4,8] * z8) % d != 0
+                            continue
+                        end
+                        for z3 in zlist
+                            if D[3,3] * abs2(z3) + D[4,4] * abs2(z4) + D[5,5] * abs2(z5) + D[6,6] * abs2(z6) + D[7,7] * abs2(z7) + D[8,8] * abs2(z8) > normbound
                                 continue
                             end
-                            G = Complex{Int}[z1 z2 z3 z4]
-                            ndT = dT + G' * D[i, i] * G
-                            nG = zeros(Complex{Int}, 8, 4)
-                            nG[i, :] = G
-                            nLiG = (LiG + Li * nG) % d
-                            p = Pair(ndT, nLiG)
-                            m1[p] = get(m1, p, 0) + a
+                            if (Li[3,3] * z3 + Li[3,4] * z4 + Li[3,5] * z5 + Li[3,6] * z6 + Li[3,7] * z7 + Li[3,8] * z8) % d != 0
+                                continue
+                            end
+                            for z2 in zlist
+                                if D[2,2] * abs2(z2) + D[3,3] * abs2(z3) + D[4,4] * abs2(z4) + D[5,5] * abs2(z5) + D[6,6] * abs2(z6) + D[7,7] * abs2(z7) + D[8,8] * abs2(z8) > normbound
+                                    continue
+                                end
+                                if (Li[2,2] * z2 + Li[2,3] * z3 + Li[2,4] * z4 + Li[2,5] * z5 + Li[2,6] * z6 + Li[2,7] * z7 + Li[2,8] * z8) % d != 0
+                                    continue
+                                end
+                                for z1 in zlist
+                                    if D[1,1] * abs2(z1) + D[2,2] * abs2(z2) + D[3,3] * abs2(z3) + D[4,4] * abs2(z4) + D[5,5] * abs2(z5) + D[6,6] * abs2(z6) + D[7,7] * abs2(z7) + D[8,8] * abs2(z8) != normbound
+                                        continue
+                                    end
+                                    if (Li[1,1] * z1 + Li[1,2] * z2 + Li[1,3] * z3 + Li[1,4] * z4 + Li[1,5] * z5 + Li[1,6] * z6 + Li[1,7] * z7 + Li[1,8] * z8) % d != 0
+                                        continue
+                                    end
+                                    LG = [z1, z2, z3, z4, z5, z6, z7, z8]
+                                    dG = Li * LG
+                                    G = dG ÷ d
+                                    @assert d * G == dG
+                                    @assert G' * S * G == k
+                                    push!(Glist, G)
+                                end
+                            end
                         end
                     end
                 end
             end
         end
-        m0 = m1
     end
-    m = Dict{Matrix, BigInt}() # T ↦ a
-    for ((dT, LiG), a) in m0
-        if LiG != zeros(Complex{Int}, 8, 4)
-            println("error")
-        end
-        if dT % d != zeros(Complex{Int}, 4, 4)
-            println("error")
-        end
-        m[dT ÷ d] = a
-    end
-    return m
+    return Glist
 end
 
-function run(n::Int, tracebound::Int)
-    m1 = run(4, tracebound, S1, D1, L1, Li1, d1)
-    m2 = run(4, tracebound, S2, D2, L2, Li2, d2)
-    m3 = run(4, tracebound, S3, D3, L3, Li3, d3)
-    m4 = Dict{Matrix{Complex{Int}}, BigInt}()
-    for (T, a) in m1
-        println(trace(T))
-        m4[T] = get(m4, T, 0) + 8 * a
-    end
-    for (T, a) in m2
-        println(trace(T))
-        m4[T] = get(m4, T, 0) - 15 * a
-    end
-    for (T, a) in m3
-        println(trace(T))
-        m4[T] = get(m4, T, 0) + 7 * a
-    end
-    m5 = Dict{Matrix{Complex{Int}}, BigInt}()
-    for (T, a) in m4
-        if a != 0
-            m5[T] = a
+function compute(S::Matrix{Complex{Int}}, D::Matrix{Int}, L::Matrix{Complex{Int}}, Li::Matrix{Complex{Int}}, d::Int)
+    Glist = search(2, S, D, L, Li, d)
+    a4 = 0
+    i = 0
+    for G1 in Glist
+        amt = 0
+        for G2 in Glist
+            if G1' * S * G2 != 0
+                continue
+            end
+            for G3 in Glist
+                if G1' * S * G3 != 0
+                    continue
+                end
+                if G2' * S * G3 != 0
+                    continue
+                end
+                amt += 1
+                
+            end
         end
+        val = 0
+        for G4 in Glist
+            x = G1' * S * G4
+            val += x * x * x * x
+        end
+        a4 += amt * val
+        i += 1
+        println("status ", d, ": ", i, " ", amt, " ", val)
     end
-    return m5
+    return a4
 end
 
-m = run(4, 3)
-# Why is trace 3 slower than trace 2?
-# Use alternate Li1 matrix
-
-for (T, a) in m
-    println(T, ": ", a)
+function compute()
+    return 8 * compute(S1, D1, L1, Li1, d1) - 15 * compute(S2, D2, L2, Li2, d2) + 7 * compute(S3, D3, L3, Li3, d3)
 end
+
+println(compute())
